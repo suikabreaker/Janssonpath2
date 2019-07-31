605280a9d7aedf03bfa5c55543b54adec5181207
zhouxumin <zhouxumin@tcxa.com.cn>
2019-07-31 15:02:01 +0800

    fully support for muilt-byte

--- src/lexeme.c
+++ src/lexeme.c
@@ -1,4 +1,4 @@
-#include <ctype.h>
+#include <wctype.h>
 #include <stdlib.h>
 #include <string.h>
 #include "private/common.h"
@@ -13,15 +13,49 @@ static string_slice make_slice(const char* s_begin, const char* s_end) {
 
 #define s_begin (*ps_begin)
 
-static void mb_next(const char** ps_begin, const char* s_end, jsonpath_error_t* error) {
-	int len = mblen(s_begin, s_end ? s_end - s_begin : INT_MAX);
-	if (len <= 0) {
-		error->code = 0x31;
-		error->reason = strerror(errno);
-		error->extra = (void*)s_begin;
+#define IS_END(begin, end) (begin == end || !*begin)
+
+#define check_error() do{if (error->code) return empty_word;}while(0)
+
+#define go_next() do{mb_next(&s_begin, s_end, error);check_error();}while(0)
+
+static const jsonpath_error_t encode_error(const char *pos){
+	jsonpath_error_t ret = { 0x31, strerror(errno), (void*)pos };
+	return ret;
+}
+
+static const jsonpath_error_t zero_length_escape_error(const char* pos) {
+	jsonpath_error_t ret = { 0x23, "escape sequnce with zero length", (void*)pos };
+	return ret;
+}
+
+static wchar_t mb_peek;
+
+static void mb_peek_init(const char** ps_begin, const char* s_end, jsonpath_error_t* error) {
+	if (IS_END(s_begin, s_end)) {
+		mb_peek = L'\0';
+		return;
+	}
+	mbtowc(&mb_peek, s_begin, s_end ? s_end - s_begin : MB_LEN_MAX);
+	if (mb_peek == -1) {
+		*error = encode_error(s_begin);
+	}
+}
+
+static void mb_next(const char** ps_begin, const char* s_end, jsonpath_error_t* error) { // if mb_peek_init does not generate error, this should not.
+	int len = mblen(s_begin, s_end ? s_end - s_begin : MB_LEN_MAX);
+	if (len == -1) {
+		*error = encode_error(s_begin);
 		return;
 	}
 	s_begin += len;
+	mb_peek_init(ps_begin, s_end, error);
+}
+
+static wchar_t mb_read(const char** ps_begin, const char* s_end, jsonpath_error_t* error) {
+	wchar_t ret = mb_peek;
+	mb_next(ps_begin, s_end, error);
+	return ret;
 }
 
 static string_slice get_string(
@@ -30,51 +64,49 @@ static string_slice get_string(
 	assert(ps_begin && s_begin);
 	assert (s_begin != s_end && *s_begin);
 
+	mb_peek_init(ps_begin, s_end, error); // no unwanted side effect so it's not harmful to do multiple times
+
 	const char* start = s_begin;  // start of the word
-	char delima = *s_begin;
-	assert(delima == '\'' || delima == '"');
-	s_begin++;
+	wchar_t delima = mb_peek;
+	assert(delima == L'\'' || delima == L'"');
+	go_next();
 	while (true) {
-		if (s_begin == s_end ||
-			!*s_begin) {  // unexpected input s_ending before string s_ends
+		if (!mb_peek) {  // unexpected input s_ending before string s_ends
 			error->code = 0x21;
 			error->reason = "unenclosing quotation mark";
 			error->extra = (void*)start;
 			return empty_word;
-		}else if (*s_begin == '\\') {  // escape
-			s_begin++;
-			if (!*s_begin) {
+		}
+		if (mb_peek == L'\\') {  // escape
+			go_next();
+			if (!mb_peek) {
 				error->code = 0x22;
 				error->reason = "unexpected eof while encounting escape";
-				error->extra = (void*)(s_begin - 1);
+				error->extra = (void*)s_begin;
 				return empty_word;
-			}else if(*s_begin=='x'){
-				s_begin++;
+			}else if(mb_peek == L'x'){
+				go_next();
 				int i;
-				for (i = 0; i < 2 && s_begin != s_end && *s_begin && isascii(*s_begin) && isxdigit(*s_begin); i++);
+				for (i = 0; i < 2 && !mb_peek && iswxdigit(mb_peek); i++) {
+					go_next();
+				}
 				if(i==0){
-					error->code = 0x23;
-					error->reason = "zero length escape sequnce";
-					error->extra = (void*)(s_begin - 2);
+					*error = zero_length_escape_error(s_begin);
 					return empty_word;
 				}
-			}else if(isascii(*s_begin) && isdigit(*s_begin)){
+			}else if(iswdigit(mb_peek)){
 				int i;
-				for (i = 0; i < 3 && s_begin != s_end && *s_begin && isascii(*s_begin) && isxdigit(*s_begin); i++);
-				if (i == 0) {
-					error->code = 0x23;
-					error->reason = "zero length escape sequnce";
-					error->extra = (void*)(s_begin - 2);
-					return empty_word;
+				for (i = 0; i < 3 && !mb_peek && iswdigit(mb_peek); i++) {
+					go_next();
 				}
 			}else { // \t \n etc.
-				s_begin++;
+				go_next();
 			}
-		}else if (*s_begin == delima) {  // s_end of the string
-			s_begin++;
+		}else if (mb_peek == delima) {  // s_end of the string
+			go_next();
 			return make_slice(start, s_begin);
 		}else {
-			mb_next(&s_begin, s_end, error);
+			go_next();
 			if (error->code) return empty_word;
 		}
 	}
@@ -91,45 +123,50 @@ string_slice LOCAL next_lexeme(
 		error->extra = NULL;
 		return empty_word;
 	}
+
+	mb_peek_init(ps_begin, s_end, error);
+
 	*error = jsonpath_error_ok;
-	if (s_begin == s_end || !*s_begin) return empty_word; // eof
+	if (!mb_peek) return empty_word; // eof
 	const char* start = s_begin;  // start of the word
 
 	// whitespaces
-	while (s_begin != s_end && isascii(*s_begin) && isspace(*s_begin)) s_begin++;
+	while (s_begin != s_end && iswspace(*s_begin)) s_begin++;
 	if (s_begin != start) return make_slice(start, s_begin);
 
 	// identifier or something - [charactor_] [digit charactor_]*
-	if (!isascii(*s_begin) || isalpha(*s_begin) || *s_begin == '_') {
-		while ((!isascii(*s_begin)) || isalnum(*s_begin) || *s_begin == '_') {
-			mb_next(&s_begin, s_end, error);
-			if (error->code) return empty_word;
+	if (!iswascii(mb_peek) || iswalpha(mb_peek) || mb_peek == L'_') {
+		while ((!iswascii(mb_peek)) || isalnum(mb_peek) || mb_peek == L'_') {
+			go_next();
 		}
 		return make_slice(start, s_begin);
 	}
 
 	// dot or number - digit* [\.]? digit* which is not empty
-	while (s_begin != s_end && isascii(*s_begin) && isdigit(*s_begin)) s_begin++;
-	if (s_begin != s_end && (*s_begin) == '.') s_begin++;
-	while (s_begin != s_end && isascii(*s_begin) && isdigit(*s_begin)) s_begin++;
+	while (!mb_peek && iswdigit(mb_peek)) {
+		go_next();
+	}
+	if (!mb_peek && mb_peek == L'.') {
+		go_next();
+	}
+	while (!mb_peek && iswdigit(mb_peek)) {
+		go_next();
+	}
 	if (s_begin != start) return make_slice(start, s_begin);
 
 	// string
-	if (*s_begin == '\"' || *s_begin == '\'') {
+	if (mb_peek == L'\"' || mb_peek == L'\'') {
 		return get_string(&s_begin, s_end, error);
 	}
 
 	// for == >= <=, we simply consider they are pairs of word, and leave it for parser to solve the ambiguity.
 	// punctor other than string delima and dot(perhaps a number)
-	if (isascii(*s_begin) && ispunct(*s_begin)) {
-		s_begin++;
+	if (iswpunct(mb_peek)) {
+		go_next();
 		return make_slice(start, s_begin);
 	}
 
-	error->code = 0x12;
-	error->reason = "unknown type of word";
-	error->extra = (void*)start;
-	++s_begin;
+	go_next(); // some are not punct
 	return make_slice(start, s_begin);
 }
 
@@ -139,6 +176,6 @@ string_slice LOCAL next_nonspace_lexeme(
 	string_slice ret = empty_word;
 	do {
 		ret = next_lexeme(&s_begin, s_end, error);
-	} while (!IS_SLICE_EMPTY(ret) && isascii(*ret.begin) && isspace(*ret.begin));
+	} while (!IS_SLICE_EMPTY(ret) && iswspace(*ret.begin));
 	return ret;
 }
